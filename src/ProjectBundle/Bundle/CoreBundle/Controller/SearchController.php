8d915edd0f1a2656b5b65179ef58c2e588511f11
himel nag rana <himel@loosemonkies.com>
2012-12-01 02:00:12 +0600

    working on home page

---
+++ src/ProjectBundle/Bundle/CoreBundle/Controller/SearchController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ProjectBundle\Bundle\CoreBundle\Controller;
+
+
+class SearchController extends BaseController
+{
+    public function search()
+    {
+        print_r($_REQUEST);die;
+    }
+
+    public function getLocation($location, $key)
+    {
+        switch ($location) {
+            case 'district': return $this->getDistricts($key);
+                             break;
+            case 'upazila':  return $this->getUpazillas($key);
+                             break;
+            case 'union':    return $this->getUnions($key);
+                             break;
+        }
+
+        return null;
+    }
+
+    public function getDistricts($key)
+    {
+        $response = '
+        <li style="clear: both;"><a class="lianchor" id="d-0" href="javascript:void(0);">ঢাকা</a></li>
+        <li style="clear: both;"><a class="lianchor" id="d-1" href="javascript:void(0);">গাজীপুর</a></li>
+        <li style="clear: both;"><a class="lianchor" id="d-2" href="javascript:void(0);">ময়মনসিংহ</a></li>
+        <li style="clear: both;"><a class="lianchor" id="d-3" href="javascript:void(0);">জামালপুর</a></li>
+        <li style="clear: both;"><a class="lianchor" id="d-4" href="javascript:void(0);">শেরপুর</a></li>';
+
+        return $response;
+    }
+
+    public function getUpazillas($key)
+    {
+        $response = '
+        <li style="clear: both;"><a class="lianchor" id="u-o" href="javascript:void(0);">ময়মনসিংহ সদর</a></li>
+        <li style="clear: both;"><a class="lianchor" id="u-1" href="javascript:void(0);">ফুলপুর</a></li>
+        <li style="clear: both;"><a class="lianchor" id="u-2" href="javascript:void(0);">নান্দাইল</a></li>
+        <li style="clear: both;"><a class="lianchor" id="u-3" href="javascript:void(0);">গৌরীপুর</a></li>
+        <li style="clear: both;"><a class="lianchor" id="u-4" href="javascript:void(0);">হালুয়াঘাট</a></li>';
+        return $response;
+    }
+
+    public function getUnions($key)
+    {
+        $response = '
+        <li style="clear: both;"><a class="lianchor" id="un-0" href="javascript:void(0);">আকুয়া</a></li>
+        <li style="clear: both;"><a class="lianchor" id="un-1" href="javascript:void(0);">বাড়েরা</a></li>';
+
+        return $response;
+    }
+}
