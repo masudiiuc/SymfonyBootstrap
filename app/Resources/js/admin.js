27787951bcc4448cc1cf7c2b0f03046c1fa2e151
Masud <masudiiuc@gmail.com>
2012-11-30 17:30:59 +0600

    add template

--- app/Resources/js/admin.js
+++ app/Resources/js/admin.js
@@ -1,6 +1,30 @@
 var Admin = {
 
-};
+    init: function(e){
 
+        this.initActionButtons();
+    },
+
+    initActionButtons: function(){
+        var self = this;
+
+        $("#addProject").bind('click', function(e){
+            self.removeProjectListSection();
+            $("#middle-container").html( _.template(Templates.AddProject, {}) );
+
+            $("#middle-container").find("#members").select2({tags:["red", "green", "blue"]});
+            $("#middle-container").find(".datepicker").datepicker();
+        });
+
+        $("#project-list li a").bind('click', function(e){
+            $("#middle-container").html( _.template(Templates.ProjectDetails, {}) );
+        });
+    },
+
+    removeProjectListSection:function(){
+
+        $("#project-list li").removeClass('active');
+    }
+};
 
 
